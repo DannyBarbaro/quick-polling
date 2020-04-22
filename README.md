d79724bc784c62cbbb4b42b964d0c66acfd47bd0
DannyBarbaro <dxb486@case.edu>
2020-04-21 22:34:09 -0400

    added super basic setup parts

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # quick-polling
+
+A fast lightweight way to poll a group. No log in. No overhead.
+
+### Useful start up
+Currently running off this setup https://flask-sse.readthedocs.io/en/latest/quickstart.html
+Open to finding a simpler way to call the program
---
+++ sse.py
@@ -0,0 +1,42 @@
+import datetime
+from flask import Flask, render_template, request, jsonify
+from flask_sse import sse
+import string
+import random
+
+activeHosts={}
+
+app = Flask(__name__)
+app.config["REDIS_URL"] = "redis://localhost"
+app.register_blueprint(sse, url_prefix='/stream')
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/host')
+def host_page():
+    letters = string.ascii_uppercase + '0123456789'
+    code =  ''.join(random.choice(letters) for i in range(10))
+    activeHosts[code] = request.remote_addr
+    data = {}
+    data['code'] = code
+    data['host'] = activeHosts[code]
+    return render_template("host.html", data=data)
+
+@app.route('/join')
+def join_page():
+    return render_template("join.html")
+
+@app.route('/room/<code>', methods=['GET'])
+def check_for_room(code):
+    return jsonify(code in activeHosts)
+
+@app.route('/answering/<roomCode>')
+def answer_page(roomCode):
+    return render_template("answering.html", roomCode=roomCode)
+
+@app.route('/hello')
+def publish_hello():
+    sse.publish({"message": "Hello!"}, type='greeting')
+    return "Message sent!"
